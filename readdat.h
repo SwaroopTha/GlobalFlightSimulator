5a4974492b2ceda96b9940a44a01e41704e840fb
Tyler Hecht <50639776+Tyler-Hecht@users.noreply.github.com>
2022-11-08 15:02:27 -0600

    docstrings mostly

--- readdat.h
+++ readdat.h
@@ -4,8 +4,21 @@
 #include <vector>
 #include <string>
 
+/**
+ * @brief readline
+ * Gets each field in a line separated by a delimiter
+ * @param ss A stringstream with the line
+ * @param delim The delimiter, comma by default
+ * @return vector<string> A vector with each item being a field in the line
+ */
 std::vector<std::string> readline(std::stringstream & ss, char delim = ',');
 
+/**
+ * @brief validID
+ * Determines if an ID is valid (i.e. )
+ * @param id 
+ * @return bool
+ */
 bool validID(int id);
 bool validIATA(std::string iata);
 bool validLatitude(double latitude);
