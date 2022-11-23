834de6d85cbc8ce8be841a618e016c29ced979f7
wotani29 <william.otani@gmail.com>
2022-11-23 06:45:04 +0000

    BFS comit hopefully works

---
+++ src/Algorithms/bfs.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "../Graph.h"
+#include <vector>
+#include <map>
+#include <string>
+#include <queue>
+using namespace std;
+class BFS {
+public:
+//will return vectors of node IDs visiteds
+vector<int> traversalOfBFS(const Graph& g, int startID, int endID);
+vector<int> getPath();
+void setAllFalse(const Graph& g);
+private:
+queue<int> queued;
+map<int, bool> visited;
+vector<int> pathOfBFS;
+};
