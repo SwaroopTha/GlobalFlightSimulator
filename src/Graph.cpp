df50a34bb26a606b5e322d9d62e0890c66f7aed7
Tyler Hecht <50639776+Tyler-Hecht@users.noreply.github.com>
2022-11-24 01:56:34 +0000

    cleaner code

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -5,14 +5,15 @@
 using namespace std;
 
 double Graph::distance(int id1, int id2) const {
-    GraphNode a = nodes.at(id1);
-    GraphNode b = nodes.at(id2);
+    double PI = 3.1415;
+    GraphNode a = nodes_.at(id1);
+    GraphNode b = nodes_.at(id2);
     //uses haversine formula
     double EARTH_RADIUS = 6378.1;
-    double lat1 = a.latitude * 3.14/180;
-    double lat2 = b.latitude * 3.14/180;
+    double lat1 = a.latitude_ * PI/180;
+    double lat2 = b.latitude_ * PI/180;
     double deltalat = lat1 - lat2;
-    double deltalong = (b.longitude - a.longitude)* 3.14/180;
+    double deltalong = (b.longitude_ - a.longitude_)* PI/180;
     double haversine1 = pow(sin(deltalat/2),2)+cos(lat1)*cos(lat2)*pow(sin(deltalong/2),2);
     double haversine2 = 2*atan2(sqrt(haversine1), sqrt(1-haversine1));
     return haversine2 * EARTH_RADIUS;
@@ -20,22 +21,22 @@ double Graph::distance(int id1, int id2) const {
 
 void Graph::addNode(int id, string name, double latitude, double longitude) {
     GraphNode node = GraphNode(id, name, latitude, longitude);
-    nodes[id] = node;
+    nodes_[id] = node;
 }
 
 bool Graph::connect(int id1, int id2) {
-    if (nodes.find(id1) == nodes.end() || nodes.find(id2) == nodes.end()) {
+    if (nodes_.find(id1) == nodes_.end() || nodes_.find(id2) == nodes_.end()) {
         //if either node doesn't exist
         return false;
     }
-    nodes[id1].connections[id2] = distance(id1, id2);
-    numConnections++;
+    nodes_[id1].connections_[id2] = distance(id1, id2);
+    numConnections_++;
     return true;
 }
 
 vector<int> Graph::getIDs() const {
     vector<int> ids;
-    for (auto it = nodes.begin(); it != nodes.end(); it++) {
+    for (auto it = nodes_.begin(); it != nodes_.end(); it++) {
         ids.push_back(it->first);
     }
     return ids;
@@ -43,8 +44,8 @@ vector<int> Graph::getIDs() const {
 
 vector<int> Graph::getConnections(int id) const {
     vector<int> ids;
-    GraphNode gn = nodes.at(id);
-    for (auto it = gn.connections.begin(); it != gn.connections.end(); it++) {
+    GraphNode gn = nodes_.at(id);
+    for (auto it = gn.connections_.begin(); it != gn.connections_.end(); it++) {
         ids.push_back(it->first);
     }
     return ids;
