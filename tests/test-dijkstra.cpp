6ddd938404e6dcdd7a0ebb02b5405e4aac0c20f4
SwarooopTha <sthamm2@illinois.edu>
2022-11-24 00:31:14 +0000

    Dijkstra finalized cases

--- tests/test-dijkstra.cpp
+++ tests/test-dijkstra.cpp
@@ -9,23 +9,64 @@
 using namespace std;
 
 TEST_CASE("Simple Dijkstra") {
+
+    cout << "------------------------------------------------" << endl;
+
+    cout << "starting [Simple Dijkstra]" << endl;
+
     Graph g;
 
     g.addNode(1, "one", 0, 1);
     g.addNode(2, "two", 0, 2);
     g.addNode(3, "three", 0, 3);
     g.addNode(4, "four", 1, 2);
+
+
     g.connect(1, 2);
     g.connect(2, 3);
 
     Dijkstras dij;
     vector<int> actual = dij.getPath(g, 1, 3);
-    vector<int> expected = {-1, 1, 2, 3};
+    vector<int> expected = {1, 2, 3};
+
+    REQUIRE(actual == expected);
+}
+
+TEST_CASE("Complex Dijkstra") {
+
+    cout << "------------------------------------------------" << endl;
+
+    cout << "starting [Complex Dijkstra]" << endl;
+    Graph g;
+
+    g.addNode(1, "one", 0, 0);
+    g.addNode(2, "two", 1, 1);
+    g.addNode(3, "three", 2, 1);
+    g.addNode(4, "four", 1, -1);
+    g.addNode(5, "five", 2, -1);
+    g.addNode(6, "six", 2, -2);
+
+    g.connect(1, 2);
+    g.connect(1, 4);
+    g.connect(2, 4);
+    g.connect(2, 3);
+    g.connect(4, 5);
+    g.connect(3, 5);
+    g.connect(5, 6);
+
+    Dijkstras dij;
+    vector<int> actual = dij.getPath(g, 1, 6);
+    vector<int> expected = {1, 4, 5, 6};
 
     REQUIRE(actual == expected);
 }
 
 TEST_CASE("No Path") {
+
+
+    cout << "------------------------------------------------" << endl;
+
+    cout << "starting [No Path]" << endl;
     Graph g;
 
     g.addNode(1, "one", 0, 1);
@@ -40,17 +81,19 @@ TEST_CASE("No Path") {
 
     Dijkstras dij;
     vector<int> actual = dij.getPath(g, 1, 5);
-    vector<int> expected = {-1, 1, 2, 3};
+    // vector<int> expected = {1, 2, 3};
 
     REQUIRE(actual == vector<int>());
     REQUIRE(dij.shortestDistance() == std::numeric_limits<double>::infinity());
 }
 
-TEST_CASE("Dijkstra Airport Simple") {
+TEST_CASE("Dijkstra Airport Nearby") {
 
     Dijkstras canvas;
 
-    
+    cout << "------------------------------------------------" << endl;
+
+    cout << "starting [Dijkstra Airport Nearby]" << endl;
 
     Graph g = readData();
 
@@ -72,11 +115,14 @@ TEST_CASE("Dijkstra Airport Simple") {
 
     for (auto i : vec1) {
         if (i == -1) continue;
-        cout << g.getName(i) << "-->" << std::endl;
+        cout << g.getName(i) << " ---> ";
     }
+    std::cout << "arrived to " << g.getName(ohare) << std::endl;
     // std::cout << ports[target] << std::endl;
 
-    vector<int> expected = {-1, 4049, 3830};
+    vector<int> expected = {4049, 3830};
+
+    cout << dij.shortestDistance() << endl;
 
     REQUIRE(vec1 == expected);
 
@@ -84,7 +130,14 @@ TEST_CASE("Dijkstra Airport Simple") {
 
 }
 
+
 TEST_CASE("Dijkstra Airport Complex") {
+
+    cout << "------------------------------------------------" << endl;
+
+    cout << "starting [Dijkstra Airport Complex]" << endl;
+
+
     Dijkstras canvas;
     Graph g = readData();
     Dijkstras dij;
@@ -111,14 +164,20 @@ TEST_CASE("Dijkstra Airport Complex") {
 
     for (auto i : journey) {
         if (i == -1) continue;
-        cout << g.getName(i) << "-->" << std::endl;
+        cout << g.getName(i) << " ---> ";
     }
-    // std::cout << g.getNode(hgk).name << std::endl;
+    std::cout << "arrived to " << g.getName(hgk) << std::endl;
 
     REQUIRE(actual > real);
+    // vector<int> expected = {-1, 4049, 3670, 3690, 193, 1382, 3077};
 
-    vector<int> expected = {-1, 4049, 3830, 3077};
+    vector<int> expected = {4049, 3830, 3077};
+
+    // vector<int> expected = {-1};
+
+    cout << dij.shortestDistance() << endl;
+
 
     REQUIRE(journey == expected);
-} 
+}
 
