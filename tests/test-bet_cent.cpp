2979c09f2a8820f6dba25ad9e56f9c4b7dd24a45
Rohan G <gvrohan11@gmail.com>
2022-12-08 21:28:38 +0000

    added test cases

--- tests/test-bet_cent.cpp
+++ tests/test-bet_cent.cpp
@@ -35,11 +35,6 @@ TEST_CASE("Betweenness Centrality Dijkstra Simple") {
 
     map<int, int> test = betcent.getAllScores(g);
 
-    // map<int, int> expected;
-    // expected[1] = 0;
-    // expected[2] = 0;
-    // expected[3] = 2;
-
     REQUIRE(test[1] == 0);
     REQUIRE(test[2] == 2);
     REQUIRE(test[3] == 3);
@@ -84,23 +79,6 @@ TEST_CASE("Min Frequency Simple") {
     REQUIRE(airport_freq_test2.find(1) == airport_freq_test2.end());
     REQUIRE(airport_freq_test2.find(4) == airport_freq_test2.end());
 
-    // // Min Frequency = 0 (Include all airports)
-    // set<int> airport_freq_test3 = betcent.getAirportsWithMinFrequency(0);
-
-    // REQUIRE((int) airport_freq_test3.size() == 4);
-    // REQUIRE(airport_freq_test3.find(2) != airport_freq_test3.end());
-    // REQUIRE(airport_freq_test3.find(3) != airport_freq_test3.end());
-    // REQUIRE(airport_freq_test3.find(1) != airport_freq_test3.end());
-    // REQUIRE(airport_freq_test3.find(4) != airport_freq_test3.end());
-
-    // // Min Frequency = 5 (Include no airports)
-    // set<int> airport_freq_test4 = betcent.getAirportsWithMinFrequency(5);
-
-    // REQUIRE((int) airport_freq_test4.size() == 0);
-    // REQUIRE(airport_freq_test4.find(2) == airport_freq_test4.end());
-    // REQUIRE(airport_freq_test4.find(3) == airport_freq_test4.end());
-    // REQUIRE(airport_freq_test4.find(1) == airport_freq_test4.end());
-    // REQUIRE(airport_freq_test4.find(4) == airport_freq_test4.end());
 
 }
 
@@ -163,6 +141,51 @@ TEST_CASE("Betweeness Centrality Dijkstra Complex") {
 
 }
 
+TEST_CASE("Min Frequency Complex") {
+    Graph g;
+
+    g.addNode(1, "air1", 0, 1);
+    g.addNode(2, "air2", 0, 2);
+    g.addNode(3, "air3", 0, 3);
+    g.addNode(4, "air4", 0, 4);
+    g.addNode(5, "air5", 0, 5);
+    g.addNode(6, "air6", 0, 6);
+    g.addNode(7, "air7", 0, 7);
+    g.addNode(8, "air8", 0, 8);
+
+    g.connect(1, 2);
+    g.connect(2, 1);
+    g.connect(2, 3);
+    g.connect(3, 2);
+    g.connect(3, 4);
+    g.connect(4, 3);
+    g.connect(3, 5);
+    g.connect(5, 3);
+    g.connect(4, 5);
+    g.connect(5, 4);
+    g.connect(5, 6);
+    g.connect(6, 5);
+    g.connect(6, 7);
+    g.connect(7, 6);
+    g.connect(5, 8);
+    g.connect(8, 5);
+
+    BetweenessCentrality betcent;
+
+    map<int, int> test = betcent.getAllScores(g);
+
+    set<int> hubs = betcent.getAirportsWithMinFrequency(12);
+
+    REQUIRE(hubs.find(1) == hubs.end());
+    REQUIRE(hubs.find(2) != hubs.end());
+    REQUIRE(hubs.find(3) != hubs.end());
+    REQUIRE(hubs.find(4) == hubs.end());
+    REQUIRE(hubs.find(5) != hubs.end());
+    REQUIRE(hubs.find(6) != hubs.end());
+    REQUIRE(hubs.find(7) == hubs.end());
+    REQUIRE(hubs.find(8) == hubs.end());
+}
+
 TEST_CASE("Betweeness Centrality Dijkstra Unconnected") {
 
     /*
@@ -209,4 +232,41 @@ TEST_CASE("Betweeness Centrality Dijkstra Unconnected") {
     REQUIRE(test[6] == 0);
     REQUIRE(test[7] == 0);
 
+}
+
+TEST_CASE("Min Frequency Unconnected") {
+    Graph g;
+
+    g.addNode(1, "air1", 0, 1);
+    g.addNode(2, "air2", 0, 2);
+    g.addNode(3, "air3", 0, 3);
+    g.addNode(4, "air4", 0, 4);
+    g.addNode(5, "air5", 0, 5);
+    g.addNode(6, "air6", 0, 6);
+    g.addNode(7, "air7", 0, 7);
+
+    g.connect(1,2);
+    g.connect(2,1);
+    g.connect(2,3);
+    g.connect(3,2);
+
+    g.connect(6,5);
+    g.connect(5,7);
+    g.connect(7,5);
+    g.connect(5,4);
+    g.connect(4,5);
+
+    BetweenessCentrality betcent;
+
+    map<int, int> test = betcent.getAllScores(g);
+
+    set<int> hubs = betcent.getAirportsWithMinFrequency(2);
+
+    REQUIRE(hubs.find(1) == hubs.end());
+    REQUIRE(hubs.find(2) != hubs.end());
+    REQUIRE(hubs.find(3) == hubs.end());
+    REQUIRE(hubs.find(4) == hubs.end());
+    REQUIRE(hubs.find(5) != hubs.end());
+    REQUIRE(hubs.find(6) == hubs.end());
+    REQUIRE(hubs.find(7) == hubs.end());
 }
