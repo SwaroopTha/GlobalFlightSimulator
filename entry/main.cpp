817302219bf0bccb3faaee191759f5fd7e660200
Tyler Hecht <50639776+Tyler-Hecht@users.noreply.github.com>
2022-12-11 20:22:46 +0000

    mainpersonal removed

--- entry/main.cpp
+++ entry/main.cpp
@@ -1,32 +1,25 @@
-#include "Graph.h"
 #include "readdat.h"
 #include "Algorithms/dijkstra.h"
 #include "Algorithms/bfs.h"
 #include "Algorithms/bet_cent.h"
-// #include "Algorithms/makeimage.h"
+#include "Algorithms/makeimage.h"
 
 using namespace std;
-//using cs225::PNG;
+using cs225::PNG;
+using cs225::HSLAPixel;
 
 int main() {
     // example usage
-    // Graph g = readData("../Data/airports.dat",  "../Data/routes.dat");
+    Graph g = readData("../Data/airports.dat",  "../Data/routes.dat");
 
-    // cout << g.size() << " airports and " << g.connections() << " connections" << endl;
+    cout << g.size() << " airports and " << g.connections() << " connections" << endl;
 
-    // int source = 3319;
-    // int target = 5554;
-    // PNG worldMap;
-    // worldMap.readFromFile("../Data/map.png");
+    int source = 3319;
+    int target = 5554;
+    PNG worldMap;
+    worldMap.readFromFile("../Data/map.png");
 
-    // cs225::PNG output = plotDijkstra(worldMap, g, source, target);
-    // output.writeToFile("output.png");
-
-    cout << "[] 0%" << endl;
-
-    cout << "[======================================================================] 100.00%" << endl;
-
-    cout << "[                                                                      ] 0.00%" << endl;
-
+    cs225::PNG output = plotDijkstra(worldMap, g, source, target, 8.5, 6, HSLAPixel(100, 0.5, 0.5), HSLAPixel(300, 0.7, 0.3));
+    output.writeToFile("output.png");
     return 0;
 }
